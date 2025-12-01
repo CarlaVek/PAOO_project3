45eb848eb2ad418e9b60ed0f46a4a84b981a6364
carla <carla.vekony@student.upt.ro>
2025-12-01 21:18:15 +0200

    unsafe_file exmple

--- src/unsafe_writer.cpp
+++ src/unsafe_writer.cpp
@@ -9,6 +9,7 @@ void studentWrite(
     std::shared_ptr<std::ofstream> file,
     std::unique_ptr<Student> student)
 {
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     (*file) << student->name() << ": " << student->message() << "\n";
 }
 
@@ -25,6 +26,7 @@ int main() {
     auto student3 = std::make_unique<Student>("Charlie", "Bob scares me sometimes");
 
     std::vector<std::thread> threads;
+
     threads.emplace_back(studentWrite, file, std::move(student1));
     threads.emplace_back(studentWrite, file, std::move(student2));
     threads.emplace_back(studentWrite, file, std::move(student3));
